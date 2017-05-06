adfae6e809520e3fe3b51907683766d281e84409
turoDog <turodog@foxmail.com>
2017-05-07 02:08:20 +0800

    选择联系人页面布局

--- app/src/main/java/com/turo/ktalk/controller/activity/PickContactActivity.java
+++ app/src/main/java/com/turo/ktalk/controller/activity/PickContactActivity.java
@@ -2,14 +2,26 @@ package com.turo.ktalk.controller.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.turo.ktalk.R;
 //选择联系人页面
 public class PickContactActivity extends Activity {
 
+    private TextView tv_pick_save;
+    private ListView lv_pick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_contact);
+
+        initView();
+    }
+
+    private void initView() {
+        tv_pick_save = (TextView) findViewById(R.id.tv_pick_save);
+        lv_pick = (ListView) findViewById(R.id.lv_pick);
     }
 }
