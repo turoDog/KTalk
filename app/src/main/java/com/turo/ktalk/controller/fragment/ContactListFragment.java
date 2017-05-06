ec9a6c67eeb3dd7470bae8219cc985d9850bb9ee
turoDog <turodog@foxmail.com>
2017-05-06 16:49:07 +0800

    刷新联系人信息页面

--- app/src/main/java/com/turo/ktalk/controller/fragment/ContactListFragment.java
+++ app/src/main/java/com/turo/ktalk/controller/fragment/ContactListFragment.java
@@ -9,12 +9,22 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.EaseUser;
 import com.hyphenate.easeui.ui.EaseContactListFragment;
+import com.hyphenate.exceptions.HyphenateException;
 import com.turo.ktalk.R;
 import com.turo.ktalk.controller.activity.InviteActivity;
 import com.turo.ktalk.controller.activity.addContactActivity;
+import com.turo.ktalk.model.Model;
+import com.turo.ktalk.model.bean.UserInfo;
 import com.turo.ktalk.utils.Constant;
 import com.turo.ktalk.utils.SpUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by YQ950209 on 2017/4/24.
@@ -68,7 +78,7 @@ public class ContactListFragment extends EaseContactListFragment {
 
         // 初始化红点显示
         boolean isNewInvite = SpUtils.getInstance().getBoolean(SpUtils.IS_NEW_INVITE, false);
-        iv_contact_red.setVisibility(isNewInvite? View.VISIBLE : View.GONE);
+        iv_contact_red.setVisibility(isNewInvite ? View.VISIBLE : View.GONE);
 
         // 邀请信息条目点击事件
         ll_contact_invite.setOnClickListener(v -> {
@@ -85,6 +95,79 @@ public class ContactListFragment extends EaseContactListFragment {
         // 注册广播
         mLocalBroadcastManager = LocalBroadcastManager.getInstance(getActivity());
         mLocalBroadcastManager.registerReceiver(ContactInviteChangeReceiver, new IntentFilter(Constant.CONTACT_INVITE_CHANGED));
+
+        // 从环信服务器获取所有的联系人信息
+        getContactFromHxServer();
+    }
+
+    private void getContactFromHxServer() {
+        Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 获取到所有的好友的环信id
+                    List<String> hxids = EMClient.getInstance().contactManager().getAllContactsFromServer();
+
+                    // 校验
+                    if (hxids != null && hxids.size() >= 0) {
+
+                        List<UserInfo> contacts = new ArrayList<UserInfo>();
+
+                        // 转换
+                        for (String hxid : hxids) {
+                            UserInfo userInfo = new UserInfo(hxid);
+                            contacts.add(userInfo);
+                        }
+
+                        // 保存好友信息到本地数据库
+                        Model.getInstance().getDbManager().getContactTableDao().saveContacts(contacts, true);
+
+                        if (getActivity() == null) {
+                            return;
+                        }
+
+                        // 刷新页面
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // 刷新页面的方法
+                                refreshContact();
+                            }
+                        });
+
+                    }
+
+                } catch (HyphenateException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    // 刷新页面
+    private void refreshContact() {
+
+        // 获取数据
+        List<UserInfo> contacts = Model.getInstance().getDbManager().getContactTableDao().getContacts();
+
+        // 校验
+        if (contacts != null && contacts.size() >= 0) {
+
+            // 设置数据
+            Map<String, EaseUser> contactsMap = new HashMap<>();
+
+            // 转换
+            for (UserInfo contact : contacts) {
+                EaseUser easeUser = new EaseUser(contact.getHxid());
+
+                contactsMap.put(contact.getHxid(), easeUser);
+            }
+
+            setContactsMap(contactsMap);
+
+            // 刷新页面
+            refresh();
+        }
     }
 
     @Override
