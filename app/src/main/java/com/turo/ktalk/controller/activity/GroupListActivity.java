e1ed23a18d95a97f0376d5194641220053b6973e
turoDog <turodog@foxmail.com>
2017-05-07 00:46:41 +0800

    从环信服务器获取群组联系人信息及条目的点击事件

--- app/src/main/java/com/turo/ktalk/controller/activity/GroupListActivity.java
+++ app/src/main/java/com/turo/ktalk/controller/activity/GroupListActivity.java
@@ -1,12 +1,23 @@
 package com.turo.ktalk.controller.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMGroup;
+import com.hyphenate.easeui.EaseConstant;
+import com.hyphenate.exceptions.HyphenateException;
 import com.turo.ktalk.R;
 import com.turo.ktalk.controller.adapter.GroupListAdapter;
+import com.turo.ktalk.model.Model;
+
+import java.util.List;
 
 /**
  * 群组列表页面
@@ -24,6 +35,34 @@ public class GroupListActivity extends Activity {
         initView();
 
         initData();
+
+        initListener();
+    }
+
+    private void initListener() {
+        // listview条目点击事件
+        lv_grouplist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Log.e("TAG", "positon:"+position);
+
+                if(position == 0) {
+                    return;
+                }
+
+                Intent intent = new Intent(GroupListActivity.this, ChatActivity.class);
+
+                // 传递会话类型
+                intent.putExtra(EaseConstant.EXTRA_CHAT_TYPE, EaseConstant.CHATTYPE_GROUP);
+
+                // 群id
+                EMGroup emGroup = EMClient.getInstance().groupManager().getAllGroups().get(position - 1);
+                intent.putExtra(EaseConstant.EXTRA_USER_ID, emGroup.getGroupId());
+
+                startActivity(intent);
+            }
+        });
     }
 
     private void initView() {
@@ -40,6 +79,46 @@ public class GroupListActivity extends Activity {
         groupListAdapter = new GroupListAdapter(this);
 
         lv_grouplist.setAdapter(groupListAdapter);
+
+        // 从环信服务器获取所有群的信息
+        getGroupsFromServer();
+    }
+
+    private void getGroupsFromServer() {
+        Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 从网络获取数据
+                    final List<EMGroup> mGroups = EMClient.getInstance().groupManager().getJoinedGroupsFromServer();
+
+                    // 更新页面
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(GroupListActivity.this, "加载群信息成功", Toast.LENGTH_SHORT).show();
+
+//                            groupListAdapter.refresh(mGroups);
+                            // 刷新
+                            refresh();
+                        }
+                    });
+                } catch (HyphenateException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(GroupListActivity.this, "加载群信息失败", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    // 刷新
+    private void refresh() {
+        groupListAdapter.refresh(EMClient.getInstance().groupManager().getAllGroups());
     }
 
 }
