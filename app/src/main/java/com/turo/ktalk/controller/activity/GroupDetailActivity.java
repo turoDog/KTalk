fc9f84e8f45f88f0356249c5bb599ebcac8b1ce5
turoDog <turodog@foxmail.com>
2017-05-07 22:20:49 +0800

    初始化button按钮的显示及退群广播

--- app/src/main/java/com/turo/ktalk/controller/activity/GroupDetailActivity.java
+++ app/src/main/java/com/turo/ktalk/controller/activity/GroupDetailActivity.java
@@ -1,16 +1,27 @@
 package com.turo.ktalk.controller.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMGroup;
+import com.hyphenate.exceptions.HyphenateException;
 import com.turo.ktalk.R;
+import com.turo.ktalk.model.Model;
+import com.turo.ktalk.utils.Constant;
+
 //群详情页面
 public class GroupDetailActivity extends Activity {
 
     private GridView gv_groupdetail;
     private Button bt_groupdetail_out;
+    private EMGroup mGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +29,131 @@ public class GroupDetailActivity extends Activity {
         setContentView(R.layout.activity_group_detail);
 
         initView();
+
+        getData();
+
+        initData();
     }
 
     private void initView() {
         gv_groupdetail = (GridView) findViewById(R.id.gv_groupdetail);
         bt_groupdetail_out = (Button) findViewById(R.id.bt_groupdetail_out);
     }
+
+    // 获取传递过来的数据
+    private void getData() {
+        Intent intent = getIntent();
+        String groupId = intent.getStringExtra(Constant.GROUP_ID);
+
+        if (groupId == null) {
+            return;
+        } else {
+            mGroup = EMClient.getInstance().groupManager().getGroup(groupId);
+        }
+    }
+
+    private void initData() {
+        // 初始化button显示
+        initButtonDisplay();
+    }
+
+    private void initButtonDisplay() {
+        // 判断当前用户是否是群主
+        if (EMClient.getInstance().getCurrentUser().equals(mGroup.getOwner())) {// 群主
+
+            bt_groupdetail_out.setText("解散群");
+
+            bt_groupdetail_out.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                // 去环信服务器解散群
+                                EMClient.getInstance().groupManager().destroyGroup(mGroup.getGroupId());
+
+                                // 发送退群的广播
+                                exitGroupBroatCast();
+
+                                // 更新页面
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(GroupDetailActivity.this, "解散群成功", Toast.LENGTH_SHORT).show();
+
+                                        // 结束当前页面
+                                        finish();
+                                    }
+                                });
+                            } catch (final HyphenateException e) {
+                                e.printStackTrace();
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(GroupDetailActivity.this, "解散群失败" + e.toString(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            });
+        } else {// 群成员
+            bt_groupdetail_out.setText("退群");
+
+            bt_groupdetail_out.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                // 告诉环信服务器退群
+                                EMClient.getInstance().groupManager().leaveGroup(mGroup.getGroupId());
+
+                                // 发送退群广播
+                                exitGroupBroatCast();
+
+                                // 更新页面
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(GroupDetailActivity.this, "退群成功", Toast.LENGTH_SHORT).show();
+
+                                        finish();
+                                    }
+                                });
+                            } catch (final HyphenateException e) {
+                                e.printStackTrace();
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(GroupDetailActivity.this, "退群失败" + e.toString(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            });
+
+        }
+    }
+
+    // 发送退群和解散群广播
+    private void exitGroupBroatCast() {
+        LocalBroadcastManager mlocalBroadcastManager = LocalBroadcastManager.getInstance(GroupDetailActivity.this);
+
+        Intent intent = new Intent(Constant.EXIT_GROUP);
+
+        intent.putExtra(Constant.GROUP_ID, mGroup.getGroupId());
+
+        mlocalBroadcastManager.sendBroadcast(intent);
+    }
+
+
 }
