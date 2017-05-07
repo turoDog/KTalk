a553b4abf8d009bd5d3ece10913f5b28d457c5d5
turoDog <turodog@foxmail.com>
2017-05-07 19:08:57 +0800

    跳转到群详情页面

--- app/src/main/java/com/turo/ktalk/controller/activity/ChatActivity.java
+++ app/src/main/java/com/turo/ktalk/controller/activity/ChatActivity.java
@@ -1,12 +1,17 @@
 package com.turo.ktalk.controller.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
+import com.hyphenate.chat.EMMessage;
 import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.easeui.ui.EaseChatFragment;
+import com.hyphenate.easeui.widget.chatrow.EaseCustomChatRowProvider;
 import com.turo.ktalk.R;
+import com.turo.ktalk.utils.Constant;
 
 //会话详情页面
 public class ChatActivity extends FragmentActivity {
@@ -20,6 +25,55 @@ public class ChatActivity extends FragmentActivity {
         setContentView(R.layout.activity_chat);
 
         intiData();
+
+        initListener();
+    }
+
+    private void initListener() {
+        easeChatFragment.setChatFragmentListener(new EaseChatFragment.EaseChatFragmentHelper() {
+            @Override
+            public void onSetMessageAttributes(EMMessage message) {
+
+            }
+
+            @Override
+            public void onEnterToChatDetails() {
+                Intent intent = new Intent(ChatActivity.this, GroupDetailActivity.class);
+                // 群id
+                intent.putExtra(Constant.GROUP_ID, mHxid);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAvatarClick(String username) {
+
+            }
+
+            @Override
+            public void onAvatarLongClick(String username) {
+
+            }
+
+            @Override
+            public boolean onMessageBubbleClick(EMMessage message) {
+                return false;
+            }
+
+            @Override
+            public void onMessageBubbleLongClick(EMMessage message) {
+
+            }
+
+            @Override
+            public boolean onExtendMenuItemClick(int itemId, View view) {
+                return false;
+            }
+
+            @Override
+            public EaseCustomChatRowProvider onSetCustomChatRowProvider() {
+                return null;
+            }
+        });
     }
 
     private void intiData() {
---
+++ app/src/main/java/com/turo/ktalk/controller/activity/GroupDetailActivity.java
@@ -0,0 +1,15 @@
+package com.turo.ktalk.controller.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.turo.ktalk.R;
+//群详情页面
+public class GroupDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_detail);
+    }
+}
